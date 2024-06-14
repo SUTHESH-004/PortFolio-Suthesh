e0f6784f32bbf14ade710528db6be6356602c943
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-06-14 17:54:41 +0530

    Today i align the background images and designed the nav-bar

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 console.log(document.documentElement.clientHeight);
 console.log(document.documentElement.clientWidth);
+//for changing the dino svg to my picture
 const myimage1 = document.querySelector(".myimage");
 const myimage2 = document.querySelector(".myimage-2");
-myimage1.addEventListener("click", function () {
+myimage1.addEventListener("mouseover", function () {
   myimage1.classList.add("hidden");
   myimage2.classList.remove("hidden");
 });
-myimage2.addEventListener("click", function () {
+myimage2.addEventListener("mouseout", function () {
   myimage1.classList.remove("hidden");
   myimage2.classList.add("hidden");
 });
-// const myimageswapper = function()
+
+//temporary hide for debugging
+const nav = document.querySelector("nav");
+nav.classList.add("hidden");
+
+
+
+
+
+
+
+
