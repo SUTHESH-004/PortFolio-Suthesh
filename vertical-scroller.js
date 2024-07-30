b5d6ae7c100ddec148d89a5508f5b18ed05ba613
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-07-30 07:07:31 +0530

    added background effect to my portfolio

--- vertical-scroller.js
+++ vertical-scroller.js
@@ -1,19 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const fullbody = document.querySelector(".fullbody");
-  let scrollAmount = 0;
+const addVerticalScroller = async function () {
+  document.addEventListener("DOMContentLoaded", () => {
+    if (window.innerWidth < 700) return;
+    const fullbody = document.querySelector(".fullbody");
+    let scrollAmount = 0;
+    window.addEventListener("wheel", (event) => {
+      // event.preventDefault();
+      scrollAmount += event.deltaY;
+      console.log(scrollAmount, event.deltaY);
+      // Limit scrolling to the number of sections
+      const maxScroll = fullbody.scrollWidth - window.innerWidth;
+      if (scrollAmount < 0) scrollAmount = 0;
+      if (scrollAmount > maxScroll) scrollAmount = maxScroll;
 
-  window.addEventListener("wheel", (event) => {
-    // event.preventDefault();
-    scrollAmount += event.deltaY;
-    console.log(scrollAmount, event.deltaY);
-    // Limit scrolling to the number of sections
-    const maxScroll = fullbody.scrollWidth - window.innerWidth;
-    if (scrollAmount < 0) scrollAmount = 0;
-    if (scrollAmount > maxScroll) scrollAmount = maxScroll;
-
-    fullbody.style.transform = `translateX(${-scrollAmount}px)`;
+      fullbody.style.transform = `translateX(${-scrollAmount}px)`;
+    });
   });
-});
+};
+if (window.innerWidth > 700) {
+  addVerticalScroller();
+}
 // console.log(document.documentElement.clientHeight);
 // console.log(document.documentElement.clientWidth);
 
